5202aee2b8a81f3613ea32e250f715aa121d89ce
Ryosuke IWANAGA <me@riywo.com>
2018-10-30 21:51:47 -0700

    wip

--- compiler.cpp
+++ compiler.cpp
@@ -1,122 +1,132 @@
 #include <iostream>
 #include <string>
+#include <functional>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Type.h>
 #include <llvm/IR/Verifier.h>
 #include <llvm/Support/raw_ostream.h>
 
-static llvm::LLVMContext TheContext;
-static std::unique_ptr<llvm::Module> TheModule = llvm::make_unique<llvm::Module>("llvm-forth", TheContext);
+using namespace llvm;
 
-struct Builder : llvm::IRBuilder<> {
-    Builder() : llvm::IRBuilder<>(TheContext) {}
+static LLVMContext TheContext;
+static IRBuilder<> Builder(TheContext);
+static std::unique_ptr<Module> TheModule;
 
-    llvm::GlobalVariable* createGlobalVariable(llvm::StringRef Name, llvm::Type* Type) {
-        TheModule->getOrInsertGlobal(Name, Type);
-        auto value = TheModule->getGlobalVariable(Name);
-        return value;
+const static auto IntType = Builder.getInt32Ty();
+const static auto StrType = Builder.getInt8PtrTy();
+const static auto PtrType = Builder.getInt8PtrTy();
+const static auto PtrPtrType = PtrType->getPointerTo();
+const static auto PtrPtrPtrType = PtrPtrType->getPointerTo();
+const static auto NodeType = StructType::create(TheContext, "node");
+const static auto NodePtrType = NodeType->getPointerTo();
+
+static Function* MainFunc;
+static BasicBlock* Entry;
+static BasicBlock* Next;
+static Constant* LastNode = ConstantPointerNull::get(NodePtrType);
+static std::map<std::string, Constant*> xtMap = {};
+static std::vector<BasicBlock*> NativeBlocks = {};
+
+static Constant* CreateGlobalVariable(const std::string& name, Type* type, Constant* initial,
+        bool isConstant=true, GlobalVariable::LinkageTypes linkageType=GlobalVariable::LinkageTypes::PrivateLinkage) {
+    auto g = TheModule->getOrInsertGlobal(name, type);
+    auto val = TheModule->getGlobalVariable(name);
+    val->setLinkage(linkageType);
+    val->setConstant(isConstant);
+    val->setInitializer(initial);
+    return g;
+}
+
+static void AddNativeWord(const std::string& word, const std::function<void()>& impl) {
+    auto block = BasicBlock::Create(TheContext, "i_" + word, MainFunc);
+    Builder.SetInsertPoint(block);
+    impl();
+    NativeBlocks.push_back(block);
+
+    auto xt = CreateGlobalVariable("xt_" + word, PtrType, BlockAddress::get(block));
+    xtMap[word] = xt;
+
+    auto str_ptr = Builder.CreateGlobalStringPtr(word, "k_" + word);
+    auto node_val = ConstantStruct::get(NodeType, LastNode, str_ptr, xt, ConstantPointerNull::get(PtrPtrPtrType));
+    LastNode = CreateGlobalVariable("d_" + word, NodeType, node_val);
+}
+
+static void Initialize() {
+    TheModule = make_unique<Module>("main", TheContext);
+    MainFunc = Function::Create(FunctionType::get(IntType, false), Function::ExternalLinkage, "main", TheModule.get());
+    Entry = BasicBlock::Create(TheContext, "entry", MainFunc);
+    Next = BasicBlock::Create(TheContext, "next", MainFunc);
+
+    NodeType->setBody(
+            NodePtrType,
+            StrType,
+            PtrPtrType,
+            PtrPtrPtrType);
+
+    AddNativeWord("foo", [](){
+        Builder.CreateBr(Next);
+    });
+    AddNativeWord("bar", [](){
+        Builder.CreateBr(Next);
+    });
+    AddNativeWord("exit", [](){
+        Builder.CreateRet(Builder.getInt32(0));
+    });
+}
+
+static std::vector<Constant*> MainLoop() {
+    std::string token;
+    auto code = std::vector<Constant*>();
+/**
+    while (std::cin >> token) {
+        if (token == "bye") {
+            break;
+        } else {
+            auto xt = xtMap.find(token);
+            if (xt == xtMap.end()) {
+
+            } else {
+                code.push_back(xt->second);
+            }
+        }
     }
+**/
+    code.push_back(xtMap.find("foo")->second);
+    code.push_back(xtMap.find("bar")->second);
+    code.push_back(xtMap.find("exit")->second);
 
-    llvm::Function* createMain() {
-        auto type = getFunctionType(getInt32Ty());
-        return createFunction("main", type, llvm::Function::ExternalLinkage);
-    }
+    return code;
+}
 
-    llvm::ArrayType* getArrayType(llvm::Type* Type, uint64_t Size) {
-        return llvm::ArrayType::get(Type, Size);
-    }
+static void Finalize(const std::vector<Constant*>& code) {
+    auto code_type = ArrayType::get(PtrPtrType, code.size());
+    auto code_block = CreateGlobalVariable("code", code_type, ConstantArray::get(code_type, code));
 
-    llvm::FunctionType* getFunctionType(llvm::Type* Result, llvm::ArrayRef<llvm::Type*> Params) {
-        return llvm::FunctionType::get(Result, Params, false);
-    }
+    Builder.SetInsertPoint(Entry);
+    auto start = Builder.CreateGEP(code_block, {Builder.getInt32(0), Builder.getInt32(0)}, "start");
+    auto pc = Builder.CreateAlloca(PtrPtrPtrType, nullptr, "pc");
+    auto w = Builder.CreateAlloca(PtrPtrPtrType, nullptr, "w");
+    Builder.CreateStore(start, pc);
+    Builder.CreateBr(Next);
 
-    llvm::FunctionType* getFunctionType(llvm::Type* Result) {
-        return llvm::FunctionType::get(Result, false);
+    Builder.SetInsertPoint(Next);
+    auto current_pc = Builder.CreateLoad(pc);
+    Builder.CreateStore(current_pc, w);
+    auto new_pc = Builder.CreateGEP(current_pc, Builder.getInt8(1), "new_pc");
+    Builder.CreateStore(new_pc, pc);
+    auto br = Builder.CreateIndirectBr(Builder.CreateLoad(Builder.CreateLoad(Builder.CreateLoad(w))), NativeBlocks.size());
+    for (auto block : NativeBlocks) {
+        br->addDestination(block);
     }
-
-    llvm::Function* createFunction(const llvm::Twine& Name, llvm::FunctionType* Type,
-                                   llvm::Function::LinkageTypes Linkage = llvm::Function::ExternalLinkage) {
-        return llvm::Function::Create(Type, Linkage, Name, TheModule.get());
-    }
-
-    llvm::BasicBlock* createBasicBlock(const llvm::Twine& Name, llvm::Function* Parent) {
-        return llvm::BasicBlock::Create(Context, Name, Parent);
-    }
-
-    void dump() {
-        TheModule->print(llvm::outs(), nullptr);
-    }
-
-    llvm::GlobalVariable* createXT(const std::string& Name, llvm::BasicBlock* Block) {
-        auto xt = createGlobalVariable(Name, getInt8PtrTy());
-        xt->setLinkage(llvm::GlobalVariable::LinkageTypes::PrivateLinkage);
-        xt->setConstant(true);
-        xt->setInitializer(llvm::BlockAddress::get(Block));
-        return xt;
-    }
-};
+}
 
 int main() {
-    Builder builder;
-
-    auto putchar = builder.createFunction("putchar", builder.getFunctionType(builder.getInt32Ty(), builder.getInt32Ty()));
-
-    auto main = builder.createMain();
-    auto entry = builder.createBasicBlock("entry", main);
-    auto top = builder.createBasicBlock("top", main);
-    auto next = builder.createBasicBlock("next", main);
-    auto i_A = builder.createBasicBlock("i_A", main);
-    auto i_B = builder.createBasicBlock("i_B", main);
-    auto i_exit = builder.createBasicBlock("i_exit", main);
-
-    auto xt_A = builder.createXT("xt_A", i_A);
-    auto xt_B = builder.createXT("xt_B", i_B);
-    auto xt_exit = builder.createXT("xt_exit", i_exit);
-
-    auto code_type = builder.getArrayType(builder.getInt8PtrTy()->getPointerTo(), 5);
-    auto code = builder.createGlobalVariable("code", code_type);
-    code->setLinkage(llvm::GlobalVariable::LinkageTypes::PrivateLinkage);
-    code->setConstant(true);
-    code->setInitializer(llvm::ConstantArray::get(code_type, {
-        xt_A,
-        xt_B,
-        xt_A,
-        xt_B,
-        xt_exit,
-    }));
-
-    builder.SetInsertPoint(entry);
-    auto start = builder.CreateGEP(code, {builder.getInt32(0), builder.getInt32(0)}, "start");
-    builder.CreateBr(top);
-
-    builder.SetInsertPoint(top);
-    auto ip = builder.CreatePHI(builder.getInt8PtrTy()->getPointerTo()->getPointerTo(), 2, "ip");
-    auto w = builder.CreateLoad(ip, "w");
-    auto addr = builder.CreateLoad(w, "addr");
-    auto new_ip = builder.CreateGEP(ip, builder.getInt32(1), "new_ip");
-    ip->addIncoming(start, entry);
-    ip->addIncoming(new_ip, next);
-    auto br = builder.CreateIndirectBr(addr, 3);
-    br->addDestination(i_A);
-    br->addDestination(i_B);
-    br->addDestination(i_exit);
-
-    builder.SetInsertPoint(next);
-    builder.CreateBr(top);
-
-    builder.SetInsertPoint(i_A);
-    builder.CreateCall(putchar, builder.getInt32(65));
-    builder.CreateBr(next);
-
-    builder.SetInsertPoint(i_B);
-    builder.CreateCall(putchar, builder.getInt32(66));
-    builder.CreateBr(next);
-
-    builder.SetInsertPoint(i_exit);
-    builder.CreateCall(putchar, builder.getInt32(10));
-    builder.CreateRet(builder.getInt32(0));
-
-    builder.dump();
+    Initialize();
+    auto code = MainLoop();
+    Finalize(code);
+    TheModule->print(outs(), nullptr);
     return 0;
 }
