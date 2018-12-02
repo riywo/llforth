9af7cd745e43728b98f706e8f39914ff2e74dc19
Ryosuke IWANAGA <me@riywo.com>
2018-12-01 23:22:42 -0800

    wip

--- dict.h
+++ dict.h
@@ -43,6 +43,7 @@ namespace dict {
     };
     static std::vector<BasicBlock*> NativeBlocks = {};
     static std::map<std::string, Word> Dictionary = {};
+    static Word Main;
 
     static Constant* GetConstantIntToXtPtr(int num) {
         return ConstantExpr::getIntToPtr(ConstantInt::get(core::IntType, num), XtPtrType);
@@ -58,42 +59,44 @@ namespace dict {
         return core::CreateGlobalVariable("xt_" + word, XtType, value);
     };
 
-    static Word AddWord(const std::string& word, Constant* xt, BlockAddress* addr, BasicBlock* block=nullptr) {
+    static Word AddWord(const std::string& name, Constant* xt, BlockAddress* addr, BasicBlock* block=nullptr) {
         Word w{xt, addr, block};
-        Dictionary[word] = w;
+        Dictionary[name] = w;
         return w;
     };
 
-    static Word AddNativeWord(const std::string& word, const std::function<void()>& impl) {
-        auto block = core::CreateBasicBlock("i_" + word, engine::MainFunction);
+    static Word AddNativeWord(const std::string& name, const std::function<void()>& impl) {
+        auto block = core::CreateBasicBlock("i_" + name, engine::MainFunction);
         core::Builder.SetInsertPoint(block);
         impl();
         NativeBlocks.push_back(block);
         auto addr = BlockAddress::get(block);
-        auto str = core::Builder.CreateGlobalStringPtr(word);
-        auto xt = AddXt(word, _LastXt, str, addr, nullptr, nullptr);
+        auto str = core::Builder.CreateGlobalStringPtr(name);
+        auto xt = AddXt(name, _LastXt, str, addr, nullptr, nullptr);
         _LastXt = xt;
-        return AddWord(word, xt, addr, block);
+        return AddWord(name, xt, addr, block);
     };
 
-    static Word AddColonWord(const std::string& word, BlockAddress* addr, std::vector<std::variant<Constant*,int>> words, bool flag=false) {
-        auto str = core::Builder.CreateGlobalStringPtr(word);
+    static Word AddColonWord(const std::string& name, BlockAddress* addr, std::vector<std::variant<Constant*,int>> words, bool flag=false) {
+        auto str = core::Builder.CreateGlobalStringPtr(name);
         auto start = InitialMemory.size();
         auto here = core::GetIndex(start);
         std::vector<Constant*> compiled_words = {};
-        for (auto word: words) {
+        for (auto w: words) {
             try {
-                auto i = std::get<int>(word);
+                auto i = std::get<int>(w);
                 compiled_words.push_back(GetConstantIntToXtPtr(start + i));
             }
             catch (const std::bad_variant_access&) {
-                compiled_words.push_back(std::get<Constant*>(word));
+                compiled_words.push_back(std::get<Constant*>(w));
             }
         }
         InitialMemory.insert(InitialMemory.end(), compiled_words.begin(), compiled_words.end());
-        auto xt = AddXt(word, _LastXt, str, addr, here, core::GetBool(flag));
+        auto xt = AddXt(name, _LastXt, str, addr, here, core::GetBool(flag));
         _LastXt = xt;
-        return AddWord(word, xt, addr);
+        auto word = AddWord(name, xt, addr);
+        if (name == "main") { Main = word; }
+        return word;
     };
 
     static Value* GetXt() {
@@ -140,7 +143,7 @@ namespace dict {
         HereValue = core::CreateGlobalVariable("here", core::IndexType, core::GetIndex(InitialMemory.size()), false);
         InitialMemory.resize(1024, ConstantPointerNull::get(XtPtrType));
         Memory = core::CreateGlobalArrayVariable("dict_memory", XtPtrType, InitialMemory, false);
-        auto start = core::Builder.CreateGEP(Memory, {core::GetIndex(0), core::GetIndex(0)});
+        auto start = core::Builder.CreateGEP(Memory, {core::GetIndex(0), GetXtColon(Main.xt)});
         core::Builder.CreateStore(start, engine::PC);
         LastXt = core::CreateGlobalVariable("last_xt", XtPtrType, _LastXt, false);
     }
